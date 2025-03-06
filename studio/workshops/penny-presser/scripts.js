363a96928dd1fe3cd4a97a167720a7ea2cdc303d
kavyaa1133 <agrak413@newschool.edu>
2025-03-06 10:09:31 -0500

    workshops
    updates

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,5 +1,6 @@
 // get the press button 
 var pressButton = document.querySelector('button[name="press"]');
+var studentSelect = document.querySelector ('select[name="student"]');
 var pennySelect = document.querySelector('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
 
@@ -8,11 +9,17 @@ console.log(pressButton);
 
 // add event listener to press button 
 pressButton.addEventListener('click', () => {
+
+    // log the student value
+    console.log(studentSelect.value);
+
     // log the penny value
     console.log(pennySelect.value);
 
     //set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
+   // pennyImage.src = 'img/' + pennySelect.value;
+
+    pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
 
     // log the penny image
     console.log(pennyImage);
